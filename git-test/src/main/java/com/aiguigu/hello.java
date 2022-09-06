50f9074c8a771a802af08a35f3802a5b9a4e582b
paul <paul@cp3.com>
2022-09-07 02:02:35 +0800

    three commit

--- git-test/src/main/java/com/aiguigu/hello.java
+++ git-test/src/main/java/com/aiguigu/hello.java
@@ -3,5 +3,6 @@ package com.aiguigu;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello git!!");
+        System.out.println("Hello git!!");
     }
 }

5a5edcfd49dcf25cf1a91c64466d7a3a5956da16
paul <paul@cp3.com>
2022-09-07 13:23:28 +0800

    hot-fix test

--- git-test/src/main/java/com/aiguigu/hello.java
+++ git-test/src/main/java/com/aiguigu/hello.java
@@ -5,5 +5,6 @@ public class hello {
         System.out.println("Hello git!!");
         System.out.println("Hello git!!");
         System.out.println("Hello git3!!");
+        System.out.println("Hello git4!!");
     }
 }
